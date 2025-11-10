5bbcbf8be8875a781b8fd358154afebdfa9bd68d
MAZE <remvze@proton.me>
2025-11-10 20:34:21 +0330

    feat: add the base of registry and doc

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,12 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/lib/layout.shared';
+import type { Metadata } from "next";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { baseOptions } from "@/lib/layout.shared";
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<"/">) {
   return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
 }
+
+export const metadata: Metadata = {
+  title: "fncn: Utility Function Registry for shadcn/ui",
+  description: "A simple helper function registry for shadcn/ui.",
+};
